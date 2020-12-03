3a1aeb5f6cde019b6fe509f89dba3d5d63573030
Roger Miret <rogerxaic@users.noreply.github.com>
2020-12-03 20:53:19 +0100

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Backing up \"$RESOURCE_NAME\" at `date +%Y/%m/%d\ %H:%M:%S`"
+
+TIME=$(date +%Y-%m-%d_%H-%M-%S)
+cd $RESOURCE_PATH
+tar -zcf $RESOURCE_NAME-${TIME}.tar.gz $RESOURCE_NAME
+mega-put -c $RESOURCE_NAME-${TIME}.tar.gz $MEGA_RESOURCE_PATH
+rm -rf $RESOURCE_NAME-${TIME}.tar.gz
